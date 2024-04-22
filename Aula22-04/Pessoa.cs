4f376cfa7f0bce7e4f719b52777bf1fd272644dc
unknown <ruan.silva149@etec.sp.gov.br>
2024-04-22 20:26:32 -0300

    first commit

---
+++ Aula22-04/Pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula22_04
+{
+    class Pessoa
+    {
+        private string nome;
+        public double salario;
+
+        public double Salario
+        {
+            get
+            {
+                return this.salario;
+            }
+
+            set
+            {
+                if (this.salario > value)
+                {
+                    Console.WriteLine("Salario atual maior que o informado");
+                } else
+                {
+                    this.salario = value;
+                    Console.WriteLine("Salario atualizado");
+                }
+            }
+        }
+        
+        public Pessoa(string nome, double salario)
+        {
+            this.nome = nome;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return this.nome;
+            }
+
+            set
+            {
+                this.nome = value;
+            }
+        }
+        
+    }
+
+}
---
+++ Aula22-04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula22_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Jeferson",1000);
+            //p.Nome = "Jeferson";
+            Console.WriteLine("Nome:{0}", p.Nome);
+            p.Salario = 1000;
+            p.Salario = 900;
+            p.Salario = 1100;
+            Console.WriteLine("Salario: {0}", p.salario);
+            Console.ReadKey();
+
+
+        }
+    }
+}
